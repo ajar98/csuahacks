e0542469ab64b7478e9551192bd070e80fc5f870
Ajay Raj <ajay@airbears2-10-142-34-154.airbears2.1918.berkeley.edu>
2016-11-04 21:58:10 -0700

    math 53 toc

--- read_toc.py
+++ read_toc.py
@@ -6,7 +6,6 @@ from os import listdir
 def read_toc(filename, dir):
     local('convert -density 300 {1}/{0} {1}/output.png'.format(filename, dir))
     for f in [f for f in listdir(dir) if 'output' in f]:
-        with open('toc.txt', 'a') as txt:
+        with open('toc1.txt', 'a') as txt:
             txt.write(pytesseract.image_to_string(Image.open('{1}/{0}'.format(f, dir))))
-
-read_toc('toc_math54.pdf', 'toc/54')
+read_toc('toc_math_53.pdf', 'toc/53')
