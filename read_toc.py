5eab29866b0b8dacc72c5359eec46364ab37a273
Ajay Raj <ajay@airbears2-10-142-34-154.airbears2.1918.berkeley.edu>
2016-11-04 21:44:16 -0700

    final reading toc pdf --> image --> text

--- read_toc.py
+++ read_toc.py
@@ -1,4 +1,12 @@
 from PIL import Image
 from pytesseract import *
+from fabric.api import local
+from os import listdir
 
-print(pytesseract.image_to_string(Image.open('toc_math54/page-0.jpg')))
+def read_toc(filename, dir):
+    local('convert -density 300 {1}/{0} {1}/output.png'.format(filename, dir))
+    for f in [f for f in listdir(dir) if 'output' in f]:
+        with open('toc.txt', 'a') as txt:
+            txt.write(pytesseract.image_to_string(Image.open('{1}/{0}'.format(f, dir))))
+
+read_toc('toc_math54.pdf', 'toc/54')
