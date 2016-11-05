cb64fe5b38acb0923bfbafd74b73912a5b10c845
Nikunj Jain <nikunj.jain@berkeley.edu>
2016-11-05 11:36:12 -0700

    Added functionality for section range parsing

--- app.py
+++ app.py
@@ -17,12 +17,14 @@ index_txt = ""
 # offset_val = offset("")
 offset_val = 16
 
+toc_lines = parser(index_txt)
 assignment_lines = parser(assignment_txt)
 _sections = text_to_sections(assignment_lines)
+p_ranges = []
 
-for section in sections:
-    
- 
+for section in _sections:
+    chapter, section_num = section.chapter, section.section_num
+    p_ranges += [page_range(index_txt, chapter, section_num)]
 
 page_min, page_max = page_range(parser('pdfs/toc.txt'), 6, 5)
 page_min, page_max = int(page_min) + offset_val, int(page_max) + offset_val
