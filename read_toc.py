ecbe9f4c36a939a1b631c0ed31b79dd2a626192c
Nikunj Jain <nikunj.jain@berkeley.edu>
2016-11-05 00:30:23 -0700

    added read index functionality

--- read_toc.py
+++ read_toc.py
@@ -2,10 +2,21 @@ from PIL import Image
 from pytesseract import *
 from fabric.api import local
 from os import listdir
+from pdftools import pdf_add 
 
-def read_toc(filename, dir):
+
+MAX_PAGE_RANGE = 25
+
+def pdf_split(filename):
+    local('touch dest.pdf')
+    pdf_add('dest.pdf', filename,[1-MAX_PAGE_RANGE], None)
+
+def read_toc(dir):
+    filename = 'dest.pdf'
     local('convert -density 300 {1}/{0} {1}/output.png'.format(filename, dir))
     for f in [f for f in listdir(dir) if 'output' in f]:
-        with open('toc1.txt', 'a') as txt:
+        with open('toc.txt', 'a') as txt:
             txt.write(pytesseract.image_to_string(Image.open('{1}/{0}'.format(f, dir))))
-read_toc('toc_math_53.pdf', 'toc/53')
+
+pdf_split('toc_math54.pdf')
+read_toc('toc/54')
