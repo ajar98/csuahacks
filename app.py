2243e1891fd1343f87f5745b7c7f93d9aa0c0b2d
Nikunj Jain <nikunj.jain@berkeley.edu>
2016-11-05 11:26:15 -0700

    Skeleton

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 from page_range import parser, page_range
 from read import read_toc, offset, pdf_split
+import sections-questions.py
 
-offset = 16
+text_pdf = ""
+assignment_pdf = ""
+
+assignment_txt = ""
+
+#######################
+
+index_pdf = ""
+index_txt = ""
+
+#######################
+
+# offset_val = offset("")
+offset_val = 16
+
+assignment_lines = parser(assignment_txt)
+_sections = text_to_sections(assignment_lines)
+
+for section in sections:
+    
+ 
 
 page_min, page_max = page_range(parser('pdfs/toc.txt'), 6, 5)
-page_min, page_max = int(page_min) + 16, int(page_max) + 16
+page_min, page_max = int(page_min) + offset_val, int(page_max) + offset_val
 pdf_split('pdfs/54/math54.pdf', 'least_squares.pdf', _min=page_min, _max=page_max)
+
 
